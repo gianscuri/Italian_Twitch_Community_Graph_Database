b26bf5ad85095671b2a17accf00e792f009a1bdd
gianscuri <gianluca.scuri@gmail.com>
2022-04-14 00:19:04 +0200

    cose

--- Twitch_API.py
+++ Twitch_API.py
@@ -32,7 +32,7 @@ def all_active_streams(client_id, access_token):
   HEAD = {'Client-ID' : client_id,
           'Authorization' :  f'Bearer {access_token}'}
 
-  for i in range(100):  ## Creazione lista di dizionari con tutti gli streamer (max 10000 streamer)
+  for i in range(200):  ## Creazione lista di dizionari con tutti gli streamer (max 20000 streamer)
     if page_streamer['pagination'] == {}:
       break
     PARAMS = {'language': 'it',
@@ -48,41 +48,42 @@ def all_active_streams(client_id, access_token):
   print(f'Numero di stream online: {num_stream}') # Numero di stream online
   return tot, num_stream
 
-def get_tasks(session):
+def name_list(tot, num_stream):
+  user_name = [tot[i]['user_name'] for i in range(num_stream)]
+  user_login = [tot[i]['user_login'] for i in range(num_stream)]
+  return user_name, user_login
+
+def get_tasks(session, user_login):
   tasks = []
 
-  streamer_list = [tot[i]['user_name'] for i in range(num_stream)]
-  #streamer_list = streamer_list[0:100]
-
-  for streamer in streamer_list:
-    URL = f'https://tmi.twitch.tv/group/user/{streamer.lower()}/chatters'
+  for login in user_login:
+    URL = f'https://tmi.twitch.tv/group/user/{login}/chatters'
     tasks.append(session.get(url = URL, ssl = False))
 
   return tasks
 
-results = []
-async def streams_with_viewer(tot, num_stream):
+async def streams_with_viewer(user_login):
+  results = []
 
   async with aiohttp.ClientSession() as session:
-    streamer_list = [tot[i]['user_name'] for i in range(num_stream)]
-    #stream_dict = {}
-
-    #for streamer in streamer_list:
-      #URL = f'https://tmi.twitch.tv/group/user/{streamer.lower()}/chatters'
-      #response = await session.get(url = URL, ssl = False)
-      #response.raise_for_status() #check for bad response
-      #response = await response.json()
-    tasks = get_tasks(session)
+    
+    tasks = get_tasks(session, user_login)
     responses = await asyncio.gather(*tasks)
 
     for response in responses:
       results.append(await response.json())
-    #  stream_dict[streamer] = {}
-    #  stream_dict[streamer]['spect'] = response['chatters']['vips'] + response['chatters']['moderators'] + response['chatters']['viewers']
-      #stream_dict[streamer]['game_name'] = list(filter(lambda person: person['user_name'] == streamer, tot))[0]['game_name']
-      #stream_dict[streamer]['viewer_count'] = list(filter(lambda person: person['user_name'] == streamer, tot))[0]['viewer_count']
-
   return results
+
+def create_dict(stream_list, list):
+  for stream, name in zip(stream_list, list):
+    stream_dict = {}
+    streamer = name
+    #print(stream['chatters']['broadcaster'])
+    #stream_dict[streamer] = {}
+    #stream_dict[streamer]['spect'] = stream['chatters']['vips'] + stream['chatters']['moderators'] + stream['chatters']['viewers']
+    #stream_dict[streamer]['game_name'] = list(filter(lambda person: person['user_name'] == streamer, tot))[0]['game_name']
+    #stream_dict[streamer]['viewer_count'] = list(filter(lambda person: person['user_name'] == streamer, tot))[0]['viewer_count']
+  #return stream_dict
 
 def save_file(dict):
   filename = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
@@ -96,10 +97,12 @@ def save_file(dict):
 client_id, client_secret = load_keys()
 access_token = request_access_token(client_id, client_secret)
 tot, num_stream = all_active_streams(client_id, access_token)
+user_name, user_login = name_list(tot, num_stream)
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) #serve per non far uscire un errore
-dict = asyncio.run(streams_with_viewer(tot, num_stream))
+stream_list = asyncio.run(streams_with_viewer(user_login))
 
 
+#dict = create_dict(stream_list, tot)
 
-save_file(str(dict))
+#save_file(str(dict))
